2a6aa62cdcb73933c2cb3754ec5056d0fc251e3c
yigui <3109746539@qq.com>
2020-07-31 16:52:57 +0800

    注册中心and考勤服务

---
+++ attends/src/main/java/com/hr/AttendsApplication.java
@@ -0,0 +1,13 @@
+package com.hr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttendsApplication.class, args);
+	}
+
+}
